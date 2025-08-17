1fe90f47433eb850ef6a346acc02f80b214b3243
Jonas Dupre <jonasdupre@yahoo.de>
2025-08-17 16:20:30 +0200

    django FinanceItem model

--- backend/fino/finance_app/models.py
+++ backend/fino/finance_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class FinanceItemType(models.Model):
+    type = models.CharField(max_length=16)
+
+class FinanceItemCategory(models.Model):
+    category = models.CharField(max_length=50)
+
+class FinanceItem(models.Model):
+    title = models.CharField(max_length=100)
+    type = models.ForeignKey(FinanceItemType, on_delete=models.SET_NULL, null=True)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateField()
+    category = models.ForeignKey(FinanceItemCategory, on_delete=models.SET_NULL, null=True)
+    notes = models.TextField(null=True, blank=True)
