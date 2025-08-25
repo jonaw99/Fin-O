a27d966611ccb28c589428c181de6debd1cfff7b
Jonas Dupre <jonasdupre@yahoo.de>
2025-08-25 22:24:23 +0200

    django FinananceItem viewset

--- backend/fino/finance_app/views.py
+++ backend/fino/finance_app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
+from rest_framework import permissions, viewsets
+
+from .serializers import FinanceItemSerializer
+from .models import FinanceItem
+
 # Create your views here.
+class FinanceItemViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows finance items to be viewed or edited.
+    """
+    queryset = FinanceItem.objects.all()
+    serializer_class = FinanceItemSerializer
+    permission_classes = [permissions.IsAuthenticated]
