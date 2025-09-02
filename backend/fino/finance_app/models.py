bf7ac398ad7a94b06cafc6e240f0d2b1df075728
Jonas Dupre <jonasdupre@yahoo.de>
2025-09-02 12:07:16 +0200

    BE update FinanceItem serializers to accept string as FinanceItemCategory

--- backend/fino/finance_app/models.py
+++ backend/fino/finance_app/models.py
@@ -5,7 +5,7 @@ class FinanceItemType(models.Model):
     type = models.CharField(max_length=16)
 
 class FinanceItemCategory(models.Model):
-    category = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, unique=True)
 
 class FinanceItem(models.Model):
     title = models.CharField(max_length=100)
--- backend/fino/finance_app/serializers.py
+++ backend/fino/finance_app/serializers.py
@@ -1,7 +1,12 @@
-from .models import FinanceItem
+from .models import FinanceItem, FinanceItemCategory
 from rest_framework import serializers
 
 class FinanceItemSerializer(serializers.ModelSerializer):
+    category = serializers.SlugRelatedField(
+        slug_field='name',
+        queryset=FinanceItemCategory.objects.all()
+    )
+    
     class Meta:
         model = FinanceItem
         fields = '__all__'
