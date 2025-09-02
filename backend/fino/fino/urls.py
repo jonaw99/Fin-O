16272db42afcdb786687c1a18cc4c857af4e77c8
Jonas Dupre <jonasdupre@yahoo.de>
2025-09-02 11:32:45 +0200

    BE swagger docs

--- backend/fino/fino/urls.py
+++ backend/fino/fino/urls.py
@@ -17,10 +17,28 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Fino API",
+        default_version='v1',
+        description="API documentation for Fino",
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
+
 
 urlpatterns = [
     # Django Admin
     path('admin/', admin.site.urls),
+    
+   path('swagger.<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+   path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+   path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     
     path('api/', include('finance_app.urls')),
 ]
